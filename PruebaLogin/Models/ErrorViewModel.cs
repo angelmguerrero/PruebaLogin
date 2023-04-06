aacae0972ef10e2c7b3296a07e5dd0d753e52d37
Angel Guerrero <angelmguerrero@gmail.com>
2023-04-06 18:46:26 -0500

    Add project files.

---
+++ PruebaLogin/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PruebaLogin.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
